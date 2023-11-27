72bb4a626749c7d3503c81ddf4e1201dab2f2d2a
ToghrulAliyev <togrulali4@gmail.com>
2023-11-28 03:41:30 +0400

    fix hover

--- src/components/Navbar/NavbarCategories/NavCategories.tsx
+++ src/components/Navbar/NavbarCategories/NavCategories.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import SubNav from "../SubNav";
 import { navItems } from "../NavCategories";
@@ -6,9 +6,8 @@ import { navItems } from "../NavCategories";
 type Props = {};
 
 const NavCategories = (props: Props) => {
-  
-
   const [openSubNav, setOpenSubNav] = useState<boolean>(false);
+  const [activeTab, setActiveTab] = useState<string>("");
   // group at parent
   // group-hover:children
   return (
@@ -23,19 +22,32 @@ const NavCategories = (props: Props) => {
           className={`text-xl  ${openSubNav ? "rotate-90" : ""} duration-300`}
         />
       </div>
-      {navItems.map((item: any) => (
-        <>
+      {navItems.map((category: any) => (
+        <React.Fragment key={category.title}>
           <div
-            onMouseOver={() => setOpenSubNav(true)}
-            onMouseLeave={() => setOpenSubNav(false)}
-            className="flex items-center h-full px-3 hover:bg-gray-200 duration-300"
+            onMouseOver={() => {
+              setOpenSubNav(true);
+              setActiveTab(category.title);
+            }}
+            onMouseLeave={() => {
+              setOpenSubNav(false);
+              setActiveTab("");
+            }}
+            className={`flex items-center h-full px-3 ${
+              activeTab === category.title ? "bg-gray-200" : ""
+            } duration-300`}
           >
-            {item.title}
+            {category.title}
           </div>
-        </>
+        </React.Fragment>
       ))}
 
-      <SubNav openSubNav={openSubNav} setOpenSubNav={setOpenSubNav} />
+      <SubNav
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+        openSubNav={openSubNav}
+        setOpenSubNav={setOpenSubNav}
+      />
     </div>
   );
 };
--- src/components/Navbar/SubNav.tsx
+++ src/components/Navbar/SubNav.tsx
@@ -6,9 +6,11 @@ import { navItems } from "./NavCategories";
 type Props = {
   openSubNav: boolean;
   setOpenSubNav: (value: boolean) => void;
+  activeTab:string;
+  setActiveTab: (value: string) => void;
 };
 
-const SubNav: FC<Props> = ({ openSubNav, setOpenSubNav }) => {
+const SubNav: FC<Props> = ({ openSubNav, setOpenSubNav, activeTab, setActiveTab }) => {
   const womenClothingSubcategory = navItems.find(
     (item) => item.title === "Women Clothing"
   );
@@ -16,6 +18,9 @@ const SubNav: FC<Props> = ({ openSubNav, setOpenSubNav }) => {
   if (!womenClothingSubcategory) {
     alert("test");
   }
+
+
+  console.log("activeTab",activeTab)
 
   return (
     <div
@@ -27,14 +32,21 @@ const SubNav: FC<Props> = ({ openSubNav, setOpenSubNav }) => {
     >
       <div className="p-10 flex overflow-hidden w-full">
         <div className="flex flex-col w-[30%]">
-          {navItems.map((category: any) => (
-            <div className="flex p-3 justify-between hover:bg-gray-300 duration-200 items-center">
+          {navItems.map((category: any) => {
+            console.log("11111111",category.title === activeTab)
+           return(
+            <div
+            key={category.title}
+            onMouseOver={() =>  setActiveTab(category.title)}
+            onMouseLeave={() =>  setActiveTab("")}
+            className={`flex p-3 justify-between  ${activeTab === category.title ? "bg-gray-200" : ""} duration-200 items-center`}>
               <div className="">{category.title}</div>
               <div>
                 <GoChevronRight />
               </div>
             </div>
-          ))}
+           )
+})}
         </div>
         <div className="h-full w-[1px] bg-gray-300 ml-4" />
         <div className="ml-12 mr-12 max-w-md w-full">
@@ -45,7 +57,7 @@ const SubNav: FC<Props> = ({ openSubNav, setOpenSubNav }) => {
           <div className="category-scrollbar flex flex-wrap overflow-y-auto">
             {womenClothingSubcategory?.subCategory?.map((subcategory: any) => {
               return (
-                <div className="flex flex-col flex-[0_0_33%] mb-3 items-center cursor-pointer ">
+                <div key={subcategory.title} className="flex flex-col flex-[0_0_33%] mb-3 items-center cursor-pointer ">
                   <div className="w-[82px] h-[82px] rounded-full bg-gray-300 overflow-hidden hover:shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] hover:scale-105 duration-300 mb-1">
                     <img
                       className="w-full h-full bg-cover"
@@ -66,13 +78,13 @@ const SubNav: FC<Props> = ({ openSubNav, setOpenSubNav }) => {
           className="w-full pr-12 pl-12 category-scrollbar  overflow-y-auto"
         >
           {womenClothingSubcategory?.detailedSubCategory?.map((detail: any) => (
-            <div className="mb-8">
+            <div key={detail.subCategoryTitle} className="mb-8">
               <div className="flex mb-4 text-xl text-red-500 items-center gap-2">
                 <h1>{detail.subCategoryTitle}</h1>
               </div>
               <div className="category-scrollbar flex flex-wrap overflow-y-auto">
                 {detail?.categoryDetails?.map((categoryDetail: any) => (
-                  <div className="flex flex-col flex-[0_0_14%] mb-3 items-center cursor-pointer ">
+                  <div key={categoryDetail.detailedSubId} className="flex flex-col flex-[0_0_14%] mb-3 items-center cursor-pointer ">
                     <div className="w-[82px] h-[82px] rounded-full bg-gray-300 overflow-hidden hover:shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] hover:scale-105 duration-300 mb-1">
                       <img
                         className="w-full h-full bg-cover"
